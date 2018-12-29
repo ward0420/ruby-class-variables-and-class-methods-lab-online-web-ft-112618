fb613db108c55f29901c3feab8e263fd0755de53
Don Ward <10is0420@gmail.com>
2018-12-29 19:48:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,17 +3,19 @@ class Song
   #attr_reader :name :artist :genre
   
   @@count = 0
+  @@all = []
   
   def initialize(name, artist, genre)
     @name = name
     @@artist = artist
     @@genre = genre
+    @@all << self
     
     @@count += 1
   end
   
   def song_artist
-    self.
+    self.all
   
   
   
