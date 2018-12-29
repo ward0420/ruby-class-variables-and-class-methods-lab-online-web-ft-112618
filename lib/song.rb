36ebeff2d0c25b86dd93e24c1590db704e702719
Don Ward <10is0420@gmail.com>
2018-12-29 19:21:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   #attr_reader :name :artist :genre
   
-  def intialize
+  def intialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
