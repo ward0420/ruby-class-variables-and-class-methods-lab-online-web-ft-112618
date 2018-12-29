daff42bd3482a08a8e45efc38d94fa37138ee8d1
Don Ward <10is0420@gmail.com>
2018-12-29 19:44:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,22 +3,17 @@ class Song
   #attr_reader :name :artist :genre
   
   @@count = 0
-  @@all = []
   
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist
-    @genre = genre
+    @@artist = artist
+    @@genre = genre
     
     @@count += 1
-    @@all << self
+
     
   end
   
-  def all_artist
-    binding.pry
-    @artist
-  end
   
   
   
