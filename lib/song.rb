2e061c2246827579a6fd5ba0b8a8df56bb868ac6
Don Ward <10is0420@gmail.com>
2018-12-29 20:36:18 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  #attr_reader :name :artist :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@artists << artist
+    @@genres << genre
+    
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+      @@genres.each_with_object(Hash.new(0)) { |genre,counts| counts[genre] += 1}
+      #binding.pry
+  end
+    
+  def self.artist_count
+    @@artists.each_with_object(Hash.new(0)) { |genre,counts| counts[genre] += 1}
+  end
+    
+  
+  
+  
+  
+end
