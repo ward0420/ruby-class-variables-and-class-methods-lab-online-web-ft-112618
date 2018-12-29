46cba4e03a6a30fb79db4840bd95c52c9dcac492
Don Ward <10is0420@gmail.com>
2018-12-29 20:34:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   end
   
   def self.genre_count
-      genres.each_with_object(Hash.new(0)) { |genre,counts| counts[genre] += 1}
+      @@genres.each_with_object(Hash.new(0)) { |genre,counts| counts[genre] += 1}
       binding.pry
     
   end
