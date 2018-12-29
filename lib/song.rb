748a316289cbb02fef3c577e47979e0fc73cc379
Don Ward <10is0420@gmail.com>
2018-12-29 19:26:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
   #attr_reader :name :artist :genre
   
+  @@count = 0
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    
+    @@count + 1
+    
   end
   
   
