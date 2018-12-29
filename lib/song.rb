9ec5fe92bd24f0132291f0f2ba0b4a91983aae12
Don Ward <10is0420@gmail.com>
2018-12-29 20:31:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,7 @@ class Song
   end
   
   def self.genre_count
-      #genres.each_with_object(Hash.new(0)) { |genre,counts| counts[genre] += 1}
-      genres.uniq.map { |x| [x, genres.count(x)] }.to_h
+      genres.each_with_object(Hash.new(0)) { |genre,counts| counts[genre] += 1}
     
   end
     
